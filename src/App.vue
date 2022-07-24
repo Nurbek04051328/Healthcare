c41a73a2d3dc76cd8435bbc8c9bce23cfd031c7b
Nurbek04051328 <bek050613@mail.ru>
2022-07-25 00:45:30 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+      <div class="left">
+        <Navbar/>
+      </div>
+      <div class="right">
+        <Sidebar/>
+        <router-view></router-view>
+      </div>
+    </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Sidebar,
+  },
+  data: () => ({
+    //
+  }),
+  created() {
+    this.$store.dispatch('getAllDoctors')
+    this.$store.dispatch('getAllPatients')
+    this.$store.dispatch('gettAllSections')
+    this.$store.dispatch('getAllChambers')
+  },
+};
+</script>
+<style >
+@import url('assets/css/style.css');
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div class="home__top">
+      <Static />
+    </div>
+    <div class="home__center">
+      <Doctor />
+    </div>
+    <div class="home__bottom">
+      <Patient />
+    </div>
+  </div>
+</template>
+
+<script>
+import Static from '../components/Static.vue'
+import Doctor from '../components/HomeDoctor.vue'
+import Patient from '../components/HomePatient.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      Static,
+      Doctor,
+      Patient
+    },
+    
+    
+  }
+</script>
